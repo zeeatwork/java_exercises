527f2039bd11e0e7a15ec41620b821b3e987247c
Zenzi Ali <zeeathome935@gmail.com>
2020-12-28 17:02:42 -0500

    constructor and inheritance practice

---
+++ src/chapter9/InheritanceTester.java
@@ -0,0 +1,9 @@
+package chapter9;
+
+public class InheritanceTester {
+    public static void main(String[] args){
+        Person person = new Person();
+        Employee employee = new Employee();
+
+    }
+}
